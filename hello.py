fd9bfdd4f72a1bd8349f951b26b6f87fa66e7074
ashika <2602396@sit.singaporetech.edu.sg>
2026-09-06 21:28:44 +0800

    Activity 3

--- hello.py
+++ hello.py
@@ -2,3 +2,22 @@ print("=========")
 print("Welcome here")
 print("My First Post!")
 print("=========") 
+
+#activity 2
+username = "scsheira"
+bio = "hii"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#activity 3
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
