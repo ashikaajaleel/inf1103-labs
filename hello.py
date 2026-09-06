99fd3d5b2bc8a07de027e9f6822879d590c64941
ashika <2602396@sit.singaporetech.edu.sg>
2026-09-06 21:24:14 +0800

    Activity 2

--- hello.py
+++ hello.py
@@ -1,12 +1,4 @@
-print("====================")
+print("=========")
 print("Welcome here")
-print("My first post!")
-print("====================")
-
-username = "scsheira"
-bio = "hii"
-followers = 100
-
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("My First Post!")
+print("=========") 
